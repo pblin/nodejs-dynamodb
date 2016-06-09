fb723bfa41a296d467679878a17658f1abd3814e
Bernard Lin <bernardlin@outlook.com>
2016-06-09 01:46:16 -0400

    fix

--- check_api.sh
+++ check_api.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-while true; do
 case "$(pidof node | wc -w)" in
 
 0)  echo "Restarting server.js:"
-    /home/ec2-user/dynamoDBAPI/run_api.sh
+    $1/run_api.sh
     ;;
-1)  echo "server.js already running"
-    sleep 5000
+1)  echo "server.js ok"
     ;;
 esac
-done &
